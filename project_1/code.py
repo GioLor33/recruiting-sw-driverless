b711d7b34d0819c721ff31b6a51142cb1a952d4a
Giorgia Lorengo <giorgia.lorengo@studenti.unitn.it>
2024-11-16 09:40:22 +0100

    Color shift problem solved

--- project_1/code.py
+++ project_1/code.py
@@ -15,6 +15,54 @@ M = np.float32([[1,0,0],[0,1,height]])
 img = cv2.warpAffine(img,M,(cols,rows))
 img[0:height, 0:cols] = top_image
 
+# STEP 2: Solve the color shift.
+
+# Since the corrupted.png file is an image which is normalized but has been 'corrupted', meaning it has 
+# a (probably different) offset in the three BGR channels, we need to first find the offset for each part of 
+# the image (lower and upper part). This can be done by comparing the color of the corrupted image with the 
+# color of the original one (which needs to be normalized).
+
+# Y=541 X=128: [250,158,3] -> due to the y value, this is in the lower part of the image
+bgr_lowerPart = np.array([250,158,3])
+color_lowerPart = img[541,128]
+offset_lowerPart = np.array([0,0,0])
+
+# Y=267 X=564: [40,195,240] -> due to the y value, this is in the upper part of the image
+bgr_upperPart = np.array([40,195,240])
+color_upperPart = img[267,564]
+offset_upperPart = np.array([0,0,0])
+
+for a in range(3):
+    offset_upperPart[a] = color_upperPart[a] - min - (max-min)*(bgr_upperPart[a])/(255)
+    offset_lowerPart[a] = color_lowerPart[a] - min - (max-min)*(bgr_lowerPart[a])/(255)
+
+# To compute the BGR values of the original image, I used the following formulas on each one of the BGR channels:
+
+# original_img_channel * (max-min) / 255 + min = normalized_img_channel
+# &
+# corrupted_img_channel = normalized_img_channel + offset => normalized_img_channel = corrupted_img_channel - offset
+
+# => original_img_channel * (max-min) / 255 + min = corrupted_img_channel - offset
+# => original_img_channel = (corrupted_img_channel - offset - min) * 255 / (max-min)
+
+# where corrupted_img_channel is given, the offset has been computed above while original_img_channel is our target.
+
+for i in range(rows):
+    offset = offset_lowerPart
+    if i < height:
+        offset = offset_upperPart
+
+    for j in range(cols):
+        for a in range(3):
+            img[i,j][a] = (img[i,j][a] - offset[a] - min)*255/(max-min)
+
+print(img[267,564])
+print(img[541,128])
+
+# As we can see, the BGR values obtained for the two points whose values where noted during the calibration 
+# procedure are (more or less) correct. In the image some artifcats appears, but they are probably due to the
+# normalization process applied on the image (compression)
+
 cv2.imshow('Image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
